f36abe125a84f68c2198fb65c2b4de882621e03b
Ben Razor <chrisbenwar1@googlemail.com>
2022-02-06 23:30:39 +0000

    Clean up code

--- web_react/src/index.js
+++ web_react/src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { AuthProvider } from "./contexts/AuthContext";
 import { ToastProvider } from 'react-toast-notifications';
 
 ReactDOM.render(
     <React.StrictMode>
-        <AuthProvider>
           <ToastProvider>
             <App/>
           </ToastProvider>
-        </AuthProvider>
     </React.StrictMode>,
     document.getElementById('root')
   );
